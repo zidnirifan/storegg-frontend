e56e3297f4ce856fe6f69ecfcfabd6c7be62f53b
Zidni Rif'an <zidni.rifan7@gmail.com>
2022-01-21 23:19:08 +0700

    refactor edit profile

---
+++ components/atoms/Input/index.tsx
@@ -0,0 +1,29 @@
+interface InputProps {
+  label: string;
+  id: string;
+  placeHolder: string;
+}
+
+export default function Input(props: InputProps) {
+  const { label, id, placeHolder, ...nativeProps } = props;
+
+  return (
+    <>
+      <label
+        for={id}
+        className="form-label text-lg fw-medium color-palette-1 mb-10"
+      >
+        {label}
+      </label>
+      <input
+        type="text"
+        className="form-control rounded-pill text-lg"
+        id={id}
+        name={id}
+        aria-describedby={id}
+        placeholder={placeHolder}
+        {...nativeProps}
+      />
+    </>
+  );
+}
